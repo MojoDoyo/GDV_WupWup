528eafed77b688dfcd592e927e94f3fef6454fed
Fleshplaquer <Janosch.Wallek@gmail.com>
2018-09-23 14:30:46 +0200

    Add files via upload
    2.

---
+++ MoyoDojo/Library/Collab/Base/Assets/Code/NPC/WaveSpawner.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform walkAgent;
+    public Transform SpawnPoint;
+    public Transform EndObjekt;
+    public GameObject PlayGround;
+    public GameObject EnemyDetect;
+    public GameObject BuildHud;
+    public GameObject Hammer;
+    public GameObject Gun;
+
+    public float PlaceX;
+    public float PlaceZ;
+    bool SpawnEnemyPhase = false;
+    bool toggle;
+   
+    private int waveNumber = 1;
+
+    public GameObject GamePhaseStartButton;
+
+    private void Awake()
+    {
+        GamePhaseStartButton.SetActive(true);
+        Hammer.SetActive(true);
+
+    }
+
+
+    private void Update()
+    {
+        if(GamePhaseStartButton.active == true && GameObject.Find("WalkAgent(Clone)") == null)
+        {
+            Instantiate(walkAgent, SpawnPoint.position, SpawnPoint.rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            toggle = !toggle;
+
+            if (toggle == true && GamePhaseStartButton.active == true)
+            { 
+                Time.timeScale = 0f;
+                Camera_Control.sensHorizontal = 0f;
+                Camera_Control.sensVertical = 0f;
+                Cursor.lockState = CursorLockMode.None;
+            }else
+            {
+                Time.timeScale = 1f;
+                Camera_Control.sensHorizontal = 8f;
+                Camera_Control.sensVertical = 8f;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+
+        }
+
+        if (SpawnEnemyPhase == true)
+        {
+            GamePhaseStartButton.SetActive(false);
+            Hammer.SetActive(false);
+            Gun.SetActive(true);
+            BuildHud.SetActive(false);
+            Time.timeScale = 1f;
+            Camera_Control.sensHorizontal = 8f;
+            Camera_Control.sensVertical = 8f;
+            Cursor.lockState = CursorLockMode.Locked;
+            SpawnWave();
+            SpawnEnemyPhase = false;
+            Debug.Log(SpawnEnemyPhase + "spawn phase");
+        }
+        
+        if (GameObject.Find("Enemy(Clone)") == null)
+        {
+            GamePhaseStartButton.SetActive(true);
+            Hammer.SetActive(true);
+            Gun.SetActive(false);
+            BuildHud.SetActive(true);
+
+            waveNumber = 1;
+            toggle = false;
+        }
+    }
+
+    public void StarteWavePhase()
+    {
+        SpawnEnemyPhase = true;
+        Destroy(GameObject.Find("WalkAgent(Clone)"));
+    }
+    
+    void SpawnWave()
+    {
+        for (int i = 3; i >= waveNumber; waveNumber++)
+        {
+            SpawnEnemy();
+        }
+        
+    }
+
+    void SpawnEnemy()
+    { 
+        Instantiate(enemyPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+
+}
---
+++ MoyoDojo/Library/Collab/Original/Assets/Code/Menu/MainMenu.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private AudioSource source;
+    private WaveSpawner wS;
+    private GameObject[] temp;
+
+
+    public AudioClip clickSound;
+    public GameObject mainMenu;
+    public GameObject OptionMenu;
+    public GameObject Copyrigth;
+    public GameObject Back;
+    public GameObject TutorialCheck;
+    public GameObject Difficulty;
+
+
+
+    public static MainMenu instance;
+
+    public enum MenuStates { MainMenu, Options, Copyrigth, Difficult };
+    public MenuStates currentState;
+
+    public static int counter = 0;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        currentState = MenuStates.MainMenu;
+        //temp = SceneManager.GetSceneByName("SampleScene").GetRootGameObjects();
+        temp = SceneManager.GetSceneAt(0).GetRootGameObjects();
+
+        if (temp.Length > 0 )
+        {
+            foreach (GameObject item in temp)
+            {
+                if (item.tag == "GameMaster")
+                {
+                    Debug.Log(item.name);
+                    wS = item.GetComponent<WaveSpawner>();
+                }
+            }
+
+        }
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case MenuStates.MainMenu:
+                mainMenu.SetActive(true);
+                OptionMenu.SetActive(false);
+                Copyrigth.SetActive(false);
+                Back.SetActive(false);
+                Difficulty.SetActive(false);
+                break;
+            case MenuStates.Options:
+                mainMenu.SetActive(false);
+                OptionMenu.SetActive(true);
+                Copyrigth.SetActive(false);
+                Back.SetActive(false);
+                Difficulty.SetActive(false);
+                break;
+            case MenuStates.Copyrigth:
+                mainMenu.SetActive(false);
+                OptionMenu.SetActive(false);
+                Copyrigth.SetActive(true);
+                Back.SetActive(true);
+                Difficulty.SetActive(false);
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    currentState = MenuStates.MainMenu;
+                }
+                break;
+             case MenuStates.Difficult:
+                mainMenu.SetActive(false);
+                TutorialCheck.SetActive(false);
+                Difficulty.SetActive(true);
+                break;
+        }
+    }
+
+    public void OnStartGame() {
+        source.PlayOneShot(clickSound);
+        if(counter >= 1)
+        {
+            currentState = MenuStates.Difficult;
+
+        }
+        else
+        {
+            TutorialCheck.SetActive(true);
+        }
+        counter++;
+    }
+
+    public void onStartEasy()
+    {
+        source.PlayOneShot(clickSound);
+        PlayerPrefs.SetInt("Dif", 0);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        Cursor.lockState = CursorLockMode.Locked;
+        TutorialCheck.SetActive(false);
+    }
+    public void onStartNormal()
+    {
+        source.PlayOneShot(clickSound);
+        PlayerPrefs.SetInt("Dif", 1);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        Cursor.lockState = CursorLockMode.Locked;
+        TutorialCheck.SetActive(false);
+    }
+    public void onStartHard()
+    {
+        source.PlayOneShot(clickSound);
+        PlayerPrefs.SetInt("Dif", 2);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        Cursor.lockState = CursorLockMode.Locked;
+        TutorialCheck.SetActive(false);
+    }
+    public void onStartImpossible()
+    {
+        source.PlayOneShot(clickSound);
+        PlayerPrefs.SetInt("Dif", 3);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        Cursor.lockState = CursorLockMode.Locked;
+        TutorialCheck.SetActive(false);
+    }
+
+    public void OnStartTutorial()
+    {
+        source.PlayOneShot(clickSound);
+        PlayerPrefs.SetInt("Dif", 0);
+        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+        Cursor.lockState = CursorLockMode.Locked;
+        TutorialCheck.SetActive(false);
+        counter++;
+    }
+
+    public void OnOptions()
+    {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Clicked Options");
+        currentState = MenuStates.Options;
+        TutorialCheck.SetActive(false);
+    }
+
+    public void OnWindowsMode()
+    {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Cklicked Window Mode");
+    }
+
+    public void OnMouseSensibility()
+    {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Sensi");
+    }
+
+    public void OnMainMenu()
+    {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Back");
+        currentState = MenuStates.MainMenu;
+    }
+
+    public void QuitGame() {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public void OpenCopyright() {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Copyright Scrollpannel open");
+        currentState = MenuStates.Copyrigth;
+        TutorialCheck.SetActive(false);
+    }
+
+    public void BacktoMenu()
+    {
+        source.PlayOneShot(clickSound);
+        Debug.Log("Back to Menu");
+        currentState = MenuStates.MainMenu;
+    }
+}
